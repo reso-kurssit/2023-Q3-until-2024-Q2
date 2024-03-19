3363d61d1294c2e7c68f14dbcc8981d9216b9061
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2024-03-19 19:17:29 +0200

    upd

--- 2024-Q2-web-ohjelmoinnin-projekti/src/components/content/Groups.jsx
+++ 2024-Q2-web-ohjelmoinnin-projekti/src/components/content/Groups.jsx
@@ -1,19 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Groups = () => {
-  return (
+  const [showCreateForm, setShowCreateForm] = useState(false);
 
+  const closeCreateForm = () => {
+    setShowCreateForm(false);
+  }
+
+  const openCreateForm = () => {
+    setShowCreateForm(true);
+  }
+
+  const search = () => {
+    // logiikka tähän
+    alert('Haku suoritettu!');
+  }
+
+  const create = () => {
+    // logiikka tähän
+    alert('Ryhmä luotu!');
+  }
+
+  return (
     <div>
       <h2>Ryhmät</h2>
+      <p className="info">
+        Kirjautuneena käyttäjänä pääset sukeltamaan syvemmälle Muuvin tarjoaman yhteisöllisyyden maailmaan. Liity olemassa oleviin ryhmiin tai luo oma, jossa voit jakaa suosikkikohtauksiasi, keskustella juonikäänteistä tai löytää samanhenkistä leffaporukkaa. Etsi ryhmiä nimillä tai tutustu kuhunkin yksityiskohtaisesti niiden omilla sivuilla.
+      </p>
 
-      <p>
-      Kaikki käyttäjät pääsevät selaamaan ryhmiä. <br/>
-      Kirjautunut käyttäjä: voi luoda uusia ryhmä "Luo ryhmä". </p>
+          <b>Etsi ryhmää</b> <br/>
+          <input className="field" type="text" placeholder="Hae ryhmää"></input>
+          <button onClick={search}>Hae</button> <br/><br/>
 
-      Kirjautunut käyttäjä pääsee ryhmän sivun kautta hallinnoimaan ryhmää olemassa olevilla oikeuksilla (vrt. ryhmän jäsen tai omistaja).<br/>
-      Tämä käsittää mm. ryhmän nimen ja kuvauksen muokkaaminen (owner), ryhmästä poistuminen (member) ja ryhmän poistaminen (owner).
+      {!showCreateForm ? (
+        <a href="#" onClick={openCreateForm}>Tai luo uusi ryhmä</a>
+      ) : (
+        <div id="createGroup">
+          <b>Uusi ryhmä</b> (<a href="#" onClick={closeCreateForm }>Sulje</a>) <br/>
+          <p>Ryhmän nimi: <input className="field" type="text" placeholder="Nimi tähän"></input></p>
+          <p>Ryhmän kuvaus: <br/>
+          <textarea className="box" type="text" placeholder="Kirjoita kuvaus"></textarea></p>
+          <button onClick={create}>Luo ryhmä</button> 
+        </div>
+      )}
+      
+      <hr />
+
+      <h3>Ryhmät listana [a - z]</h3> 
+
+      <p><a href="#">Ryhmärämä</a></p>
+      <p><a href="#">komediahullut</a></p>
+      <p><a href="#">tosimiesklubi</a></p>
+      <p><a href="#">Stadin Muuvissa</a></p>
+      <p><a href="#">SOTA-leffat</a></p>
+      <p><a href="#">Dramaqueens</a></p>
+      <p><a href="#">kauhalla kauhuu</a></p>
+      <p><a href="#">Tampereen seudun leffailijat</a></p>
+      <p><a href="#">Leffatiistai</a></p>
+      <p><a href="#">Vain-sarjoja</a></p>
+      <p><a href="#">Suomalaista filmii</a></p>
+      <p><a href="#">lastenleffat</a></p>
+
+      <hr />
+
+
     </div>
-
   );
 };
 
--- 2024-Q2-web-ohjelmoinnin-projekti/src/components/content/Reviews.jsx
+++ 2024-Q2-web-ohjelmoinnin-projekti/src/components/content/Reviews.jsx
@@ -1,21 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Reviews = () => {
+  const [showReviewForm, setShowReviewForm] = useState(false);
+
+  const closeReviewForm = () => {
+    setShowReviewForm(false);
+  }
+
+  const openReviewForm = () => {
+    setShowReviewForm(true);
+  }
+
+  const searchReviews = () => {
+    // Logiikka arvostelujen hakemiselle
+    alert('Haku suoritettu!');
+  }
+
+  const createReview = () => {
+    // Logiikka uuden arvostelun luomiselle
+    alert('Arvostelu luotu!');
+  }
+
   return (
-
     <div>
-        <h2>Arvostelut</h2>
+      <h2>Arvostelut</h2>
 
-        Kaikki: Pääsee selaamaan arvosteluja. <br/>
-        Kirjautunut käyttäjä: voi tehdä arvosteluja, "uusi arvostelu" <br/><br/>
+      <b>Etsi arvostelua</b> <br/>
+      <input className="field" type="text" placeholder="Hae arvostelua"></input>
+      <button onClick={searchReviews}>Hae</button> <br/><br/>
 
-        Arvosteluissa näytetään: tähdet, lyhyt arvosteluteksti, arvostelijan nimimerkki ja laajennettava kuvaus elokuvasta/sarjasta. <br/>
-        Ryhmät löytyvät listana ja kaikki voivat nähdä ryhmän nimen ja kuvauksen. <br/>
-        Vain kirjautuneet käyttäjät voivat liittyä ryhmiin. <br/>
-        Vain ryhmään liittynyt jäsen voi nähdä ryhmän sivun muun sisällön, esim. Chatin. <br/>
+      {!showReviewForm ? (
+        <a onClick={openReviewForm}>Tai luo uusi arvostelu</a>
+      ) : (
+        <div id="createReview">
+          <b>Uusi arvostelu</b> (<a onClick={closeReviewForm}>Sulje</a>) <br/><br/>
+
+          <b>Valitse elokuva:</b> (search) <br/>
+          <b>Anna tähdet: </b>
+          <select className="field" defaultValue="5">
+            <option value="1">&#11088; [1/5] tähteä</option>
+            <option value="2">&#11088;&#11088; [2/5] tähteä</option>
+            <option value="3">&#11088;&#11088;&#11088; [3/5] tähteä</option>
+            <option value="4">&#11088;&#11088;&#11088;&#11088; [4/5] tähteä</option>
+            <option value="5">&#11088;&#11088;&#11088;&#11088;&#11088; [5/5] tähteä</option>
+          </select>
+          <p><b>Perustelut:</b> <br/>
+          <textarea className="box" placeholder="Kirjoita arvostelu tähän"></textarea></p>
+          <button onClick={createReview}>Lähetä arvostelu</button>
+        </div>
+      )}
+
+      <hr />
+
+
+      <b>Lähetetty:</b> 00.00.2024 <br/>
+      <b>Kohde: </b> Movie or serie <br/>
+      <b>Synopsis:</b> <a href="#">Avaa</a> <br/>
+      ---------------------------------------------------------------------------- <br/>
+      <b>Käyttäjältä:</b> <i>Anonymous</i> <br/>
+      <b>Arvio:</b> &#11088;&#11088;&#11088;&#11088;&#11088; [5/5] tähteä <br/>
+      <b>Perustelut:</b> <br/>
+      Text text text text text text text text Text text text text text text text text 
+      Text text text text text text text text Text text text text text text text text Text text text text text text text text Text text text text text text text text 
+      Text text text text text text text text Text text text text text text text text Text text text text text text text text Text text text text text text text text 
+      Text text text text text text text text Text text text text text text text text Text text text text text text text text Text text text text text text text text 
+      Text text text text text text text text Text text text text text text text text <br/><br/>
+
+      <b>Lähetetty:</b> 00.00.2024 <br/>
+      <b>Kohde: </b> Movie or serie <br/>
+      <b>Synopsis:</b> <a href="#">Avaa</a> <br/>
+      ---------------------------------------------------------------------------- <br/>
+      <b>Käyttäjältä:</b> <i>Anonymous</i> <br/>
+      <b>Arvio:</b> &#11088;&#11088;&#11088;&#11088;&#11088; [5/5] tähteä <br/>
+      <b>Perustelut:</b> <br/>
+      Text text text text text text text text Text text text text text text text text 
+      Text text text text text text text text Text text text text text text text text Text text text text text text text text Text text text text text text text text 
+      Text text text text text text text text Text text text text text text text text Text text text text text text text text Text text text text text text text text 
+      Text text text text text text text text Text text text text text text text text Text text text text text text text text Text text text text text text text text 
+      Text text text text text text text text Text text text text text text text text <br/><br/>
 
     </div>
-
   );
 };
 
